1d6a9345a44946f6e15dad365876816b947ffd82
Jeff Leung <jeff.leung@scmp.com>
2019-04-19 18:18:22 +0800

    eslint

--- test/ExerciseC6A.js
+++ test/ExerciseC6A.js
@@ -1,27 +1,22 @@
 
-var Test = require('../config/testConfig.js');
+const Test = require('../config/testConfig.js')
 
 contract('ExerciseC6A', async (accounts) => {
-
-  var config;
+  let config
   before('setup contract', async () => {
-    config = await Test.Config(accounts);
-  });
+    config = await Test.Config(accounts)
+  })
 
   it('contract owner can register new user', async () => {
-    
     // ARRANGE
-    let caller = accounts[1]; // This should be config.owner or accounts[0] for registering a new user
-    let newUser = config.testAddresses[0]; 
+    const caller = accounts[1] // This should be config.owner or accounts[0] for registering a new user
+    const newUser = config.testAddresses[0]
 
     // ACT
-    await config.exerciseC6A.registerUser(newUser, false, {from: caller});
-    let result = await config.exerciseC6A.isUserRegistered.call(newUser); 
+    await config.exerciseC6A.registerUser(newUser, false, {from: caller})
+    const result = await config.exerciseC6A.isUserRegistered.call(newUser)
 
     // ASSERT
-    assert.equal(result, true, "Contract owner cannot register new user");
-
-  });
-
- 
-});
+    assert.equal(result, true, 'Contract owner cannot register new user')
+  })
+})
