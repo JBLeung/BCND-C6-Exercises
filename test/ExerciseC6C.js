1f764b80ee6c6b4109e7f757c3cd810a47d28109
Jeff Leung <jeff.leung@scmp.com>
2019-04-19 20:26:48 +0800

    fix eslint

--- test/ExerciseC6C.js
+++ test/ExerciseC6C.js
@@ -1,33 +1,27 @@
-var Test = require('../config/testConfig.js');
+const Test = require('../config/testConfig.js')
 
 contract('ExerciseC6C', async (accounts) => {
-
-  var config;
+  let config
   before('setup contract', async () => {
-    config = await Test.Config(accounts);
-  });
+    config = await Test.Config(accounts)
+  })
 
   it('can register Employee, add sale and calculate bonus', async () => {
-    
     // ARRANGE
-    let employee = {
+    const employee = {
       id: 'test1',
       isAdmin: false,
-      address: config.testAddresses[0]
-    };
-    let sale = 400;
-    let expectedBonus = parseInt(sale * 0.07);
+      address: config.testAddresses[0],
+    }
+    const sale = 400
+    const expectedBonus = parseInt(sale * 0.07)
 
     // ACT
-    await config.exerciseC6C.registerEmployee(employee.id, employee.isAdmin, employee.address);
-    await config.exerciseC6C.addSale(employee.id, 400);
-    let bonus = await config.exerciseC6C.getEmployeeBonus.call(employee.id);
+    await config.exerciseC6C.registerEmployee(employee.id, employee.isAdmin, employee.address)
+    await config.exerciseC6C.addSale(employee.id, 400)
+    const bonus = await config.exerciseC6C.getEmployeeBonus.call(employee.id)
 
     // ASSERT
-    assert.equal(bonus.toNumber(), expectedBonus, "Calculated bonus is incorrect incorrect");
-
-  });
-
-
- 
-});
+    assert.equal(bonus.toNumber(), expectedBonus, 'Calculated bonus is incorrect incorrect')
+  })
+})
