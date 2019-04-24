cb57cf52a39342d7d0b7146868f39591589464e8
Jeff Leung <jeff.leung@scmp.com>
2019-04-24 19:19:32 +0800

    add payment production pattern code

--- contracts/ExerciseC6B.sol
+++ contracts/ExerciseC6B.sol
@@ -16,6 +16,9 @@ contract ExerciseC6B {
 
 
     address private contractOwner;                  // Account used to deploy contract
+    mapping(address => uint256) private sales;
+    uint256 private enabled = block.timestamp;
+    uint256 private counter = 1;
 
 
     constructor
@@ -42,10 +45,52 @@ contract ExerciseC6B {
         _;
     }
 
+    // payment production pattern: rate limiting 
+    modifier rateLimit(unit time){
+        require(block.timestamp >= enabled, "Rate limiting in effect");
+        enabled = enabled.add(time);
+        _;
+    }
+
+    // payment production pattern: Re-entrancy Guard
+    /* The idea of entrancyGuard is this: 
+    *  When user1 calls safeWithdraw - entrancyGuard modifier, 
+    *  a local variable guard is instantiated with the value of 
+    *  counter(let's say 2). Then the _; causes the calling
+    *  function to be executed (if safeWithdraw calls an 
+    *  external contract that will call safeWithdraw again,
+    *  then on the 2nd enterance in the entrancyGuard(), the
+    *  global variable counter will be incremented again (so 3).
+    *  At the end, when require guard == counter, the guard is a
+    *  local variable, saved on method stack (so it's value will
+    *  be 2 while the counter is a global variable and its value
+    *  is 3. Thus, 2 =!3 and will trigger the revert. Regarding
+    *  concurrency issues, there is no such thing. Each transaction
+    *  is executed one at a time not in paralel (they are put in a
+    *  queue and sorted by the gas Price you're willing to pay). 
+    */
+
+    modifier entrancyGuard(){
+        counter = counter.add(1);
+        uint256 guard = counter;
+        _;
+        require(guard == counter, "That is not allowed");
+    }
+
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
 
+    // payment production pattern: checks - effects - interaction
+    function safeWithdraw(uint256 amount) external rateLimit(30 miniutes){
+        // Checks
+        require(msg.sender == tx.origin, "Contracts not allowed"); // make sure this function can call by user account but not an contact account
+        require(sales[msg.sender] >= amount, "Insufficient funds");
+        // Effects
+        sales[msg.sender] = sales[msg.sender].sub(amount);
+        // Interaction
+        msg.sender.transfer(amount);
+    }
 
     
 }
